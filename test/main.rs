e0664df912c32009be861b6a536aa987e4adb45b
freemanbach <flo@radford.edu>
2020-04-19 21:39:15 -0400

    added a few more items

--- test/main.rs
+++ test/main.rs
@@ -1,5 +1,32 @@
 
 fn main() {
+
+    // boolean
+    let a: bool = true;
+
+    // 64 bit floating point
+    let myfloat1: f64 = 1.0;  // Regular annotation
+    let myfloat2 = 1.0f64;    // Suffix annotation
+    
+
+    // mutable integer 
+    let mut myint: i32   = 5;  // regular annotation
+    myint = 90;
+
+    // non-mutable integer and floating point
+    let std_float   = 3.0; // `f64`
+    let std_integer = 7;   // `i32`
+
+    // regular fixed array
+    let std_array = [0;5];
+
+    // Fixed-size array with size
+    let xs: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // fixed-size array with fixed initialized values
+    let ys: [i32; 500] = [0; 500];
+
+
     // Integer addition
     println!("9 + 7 = {}", 9 + 7);
 
@@ -18,12 +45,10 @@ fn main() {
     println!("true OR false is {}", true || false);
     println!("NOT true is {}", !true);
 
-    // Bitwise operations
-    println!("0011 AND 0101 is {:04b}", 0b0011u32 & 0b0101);
-    println!("0011 OR 0101 is {:04b}", 0b0011u32 | 0b0101);
-    println!("0011 XOR 0101 is {:04b}", 0b0011u32 ^ 0b0101);
-    println!("1 << 5 is {}", 1u32 << 5);
-    println!("0x80 >> 2 is 0x{:x}", 0x80u32 >> 2);
+    // Array / Tuple
+    let myarray1 = (1, 4, 6, 8u64, -1, -2, -3, -8i64, 0.1f32, 0.2f64, 'a', "Hello earth", true);
+    let myarray2 = (1, 4, 6, 8u64, -1);
+    println!("print out: {:?}", myarray2);
 
     // underscores to improve readability!
     println!("One million is written as {}", 1_000_000u32);
