f2682e5db1f1f24bcac00bf3d08dd4e90e5b10f4
freeman lo <flo@radford.edu>
2023-05-10 00:48:28 -0400

    added a simple hello something project

---
+++ hello_planet/src/main.rs
@@ -0,0 +1,15 @@
+
+#![allow(unused)]
+
+// use rand::Rng;
+use std::io;
+use std::env;
+use std::fs::File;
+use std::path::Path;
+use std::convert::TryInto;
+use std::io::{Write, BufReader, BufRead, ErrorKind};
+use std::cmp::Ordering;
+
+fn main() {
+    println!("Hello, Planet Earth!");
+}
